1f2978ec8c5d68b5fdbe2aa78098848d5047c559
akhanna101 <akhanna101@gmail.com>
2020-04-08 14:09:37 -0400

    added folders for behavioral scripts. One general, one specific to Cue Map

---
+++ Cue Map Behavior Scripts/Import_CM_Py.m
@@ -0,0 +1,146 @@
+function [MAT] = Import_CM_Py(filename)
+%This function imports cue map data for the CMv1 script
+%   Detailed explanation goes here
+
+%Incase filenames are copied and pasted into the Import_CM_Py function this
+%flips the forward slash to backslash in the string. Backslashes are used
+%to avoid escape in sprintf
+filename(filename == '\') = '/';
+
+%This gets the rat num and list. First get rid of the directory info...
+bs = strfind(filename,'/');
+filen = filename(bs(end)+1:end);
+nums = regexp(filen,'\d*','Match');
+Rat_Num = str2double(nums{1});
+List_Num = str2double(nums{2});
+
+%open the file
+fileID = fopen(filename);
+header = textscan(fileID, '%s %s', 1);
+
+%This flag keeps track of whether the old version of the script is being
+%used, or the new version
+old_v = true;
+
+Reward_Zones = cell(1,6);
+%The file structure was changed on April 16th. The reward zones were added
+%to the top of the file
+if all(strncmp(header{1}, 'Reward',6))
+   header2 = textscan(fileID, '%s', 3); 
+   RZs = regexp(header2{1},'\d*','Match');
+   Reward_Zones{Rat_Num} = cellfun(@str2double,RZs);
+   %Reward_Zones{Rat_Num} = cellfun(@(x) str2double(x) + 1,RZs);
+   old_v = false;
+else
+    %The following checks the reward zones for each rat to make sure they match
+    %the vertices with 'F'. This can be turned off
+    Rew_Zones{1} = [26,69,113];%Rew_Zones{1} = [27,70,114];
+    Rew_Zones{2} = [26,69,113];%[34,63,115];
+    Rew_Zones{3} = [28,71,115];%[111,82,30];
+    Rew_Zones{4} = [26,69,113];%Rew_Zones{4} = [118,75,31];
+    Rew_Zones{5} = [39,82,126];%Rew_Zones{5} = [27,114,70];
+    Rew_Zones{6} = [15,58,102];%Rew_Zones{6} = [34,155,63];
+end   
+
+Behavior_Data = textscan(fileID, '%f %s');
+
+%load the LISTS file which contains the list information for each day
+%load('E:/Cue Map/Pi_030719_Run/Lists/LISTS_DAT.mat')
+load('C:/Users/khannaa3/Desktop/Cue Map MATLAB/Cue Map 0319/Lists/LISTS_EXTENDED_DAT.mat')
+%load('C:/Users/khannaa3/Desktop/Cue Map MATLAB/Behavioral Data Aug 2019/Latent Learning Extinction/Lists_RW/LISTS_DAT.mat')
+%load('E:/Cue Map/Pi_030719_Run/Lists_Ext_Traj_1_4/LISTS_DAT2.mat')
+
+%The actual lists should be taken from the file as the random_walk lists
+%are randomized for different calls to the Cue_Map_Counterbalance
+
+%Currently 'O' is for an output
+DAT.ins = cellfun(@(x) all(strcmp(x,'O')),Behavior_Data{2});
+DAT.outs = cellfun(@(x) all(strcmp(x,'I')),Behavior_Data{2});
+
+% DAT.ins = cellfun(@(x) all(strcmp(x,'O')),Behavior_Data{2});
+% DAT.outs = cellfun(@(x) all(strcmp(x,'I')),Behavior_Data{2});
+
+DAT.F = cellfun(@(x) all(strcmp(x,'F')),Behavior_Data{2});
+DAT.state = cellfun(@str2double,Behavior_Data{2});
+
+
+Time_Stamps = round(1000*(Behavior_Data{1} - Behavior_Data{1}(1)));
+
+[response]= Logical_On_Off(Time_Stamps(DAT.ins)/1000, Time_Stamps(DAT.outs)/1000,Time_Stamps(1)/1000,Time_Stamps(end)/1000);
+%histc of spiketimes as [response]
+state_inds = find(~isnan(DAT.state));
+
+check_rews = false;
+
+reward_inds = find(DAT.F);
+if isempty(reward_inds)
+    check_rews = true;
+end    
+
+%This pads the end of state_inds with the end of the session
+state_inds(end+1) = find(strcmp('end',Behavior_Data{2}));
+%state_inds(end+1) = numel(Behavior_Data{2}) + 1;
+
+%MAT is a structure in which the response for each trial is saved in
+%the Response field. The vertices are stored in List and Block info is
+%stored in Block
+
+n = 0;
+MAT.Block = cell(1,numel(state_inds)-1);
+for i = 1:numel(state_inds)-1
+
+    n = n + 1; 
+    
+    %The vertices need to be corrected for the counterbalancing:
+    if old_v
+        vertex = DAT.state(state_inds(i)) + 1;
+    else
+        vertex = DAT.state(state_inds(i));
+    end    
+    
+    rat_counter = rem(Rat_Num,8);
+    if rat_counter == 0
+        rat_counter = 8;
+    end    
+    ind_temp = CounterBalance_Key{rat_counter}(:,1) == vertex; %#ok<USENS>
+    vertex = CounterBalance_Key{rat_counter}(ind_temp,2);
+    MAT.Rat = Rat_Num;
+    MAT.Day = List_Num;
+    
+
+    MAT.Vertices(i) = vertex;
+    
+    %When there is only a single block, there is not a cell array of block
+    %info for each vertex, rather LISTS(n).Block is a char array of the
+    %block type
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    %NEED TO CHANGE BACK!!!
+    
+    %List_Num = 1;
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
+    
+    if ischar(LISTS(List_Num).Block)
+        MAT.Block{i} = LISTS(List_Num).Block;
+    else    
+        MAT.Block(i) = LISTS(List_Num).Block(i);
+    end    
+    MAT.Response{i} = response(Time_Stamps(state_inds(i)):Time_Stamps(state_inds(i+1)));
+    
+    if check_rews
+       MAT.Rewards(i) = any(vertex == (Rew_Zones{Rat_Num}));  
+    else
+        MAT.Rewards(i) = any(reward_inds > state_inds(i) & reward_inds < state_inds(i+1));
+    end
+    
+    if MAT.Rewards(i) > 0
+      %  a = 1
+    end    
+    MAT.Pokes(i) = sum(diff(MAT.Response{i} == 1));
+    MAT.Resp_Perc(i) = sum(MAT.Response{i}/numel(MAT.Response{i}));
+end
+
---
+++ Cue Map Behavior Scripts/Plot_CM_Py_1_Sess.m
@@ -0,0 +1,182 @@
+function [] = Plot_CM_Py_1_Sess(days,rat)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+%Source_Folder = 'E:/Cue Map/Data/0319 Data';
+Source_Folder = 'C:/Users/khannaa3/Desktop/Cue Map MATLAB/Behavioral Data Aug 2019/Recorded Animals';
+
+File_End = '.txt';
+
+
+%This gets all the files from the directory
+AllFiles = dir(Source_Folder);
+
+% %This sorts the files by animal and run
+% [~, Ind] = sort([AllFiles(:).datenum]);
+% 
+% AllFiles = AllFiles(Ind);
+
+%This puts all of the filenames into a cell array of sessions, with only
+%.txt files and files which begin with group
+Filenames = {AllFiles.name};
+
+Filenames = Filenames(~cellfun(@isempty, (strfind(Filenames, File_End))));
+
+%This finds the earliest date within the TXT_Files. All session and stage
+%information is based on this date. It is important not to include other
+%sessions within the folder which are not part of the experiment
+rat_list_nums = cellfun(@(x) regexp(x,'\d*','Match'), Filenames,'UniformOutput', false);
+
+for i = 1:numel(rat_list_nums)
+    
+    rat_num(i) = str2double(rat_list_nums{i}{1});
+    day(i) = str2double(rat_list_nums{i}{2});
+end
+
+%Blocks in the MAT struct are: 
+%V : vertical trajectory blocks
+%H : horizontal trajectory blocks
+%R : random walk blocks
+
+
+%This gets the days that should be used
+d_use = ismember(day,days);
+rat_use = rat_num == rat;
+
+Response = cell(1,3);
+Rewards = cell(1,3);
+for i = find(d_use & rat_use)
+    filename = sprintf('%s/%s',Source_Folder,Filenames{i})
+    [MAT] = Import_CM_Py(filename);
+    [Resp,Rew] = Response_MAT(MAT);
+    for j = 1:3
+        
+        Response{j} = cat(1,Response{j},Resp{j});
+        Rewards{j} =  cat(1,Rewards{j},Rew{j});
+    end
+    
+    plot_tr = false;
+    plot_rw = false;
+    
+    if any(strncmp('V',MAT.Block,1) | strncmp('H',MAT.Block,1))
+        [App_Tr,App_Tr_Rew] = Approach_MAT(MAT,{'V','H'}); 
+        plot_tr = true;
+        
+    end
+    if any(strncmp('R',MAT.Block,1))
+        [App_RW,App_RW_Rew] = Approach_MAT(MAT,{'R'}); 
+        plot_rw = true;
+    end
+    break
+end
+
+if plot_tr && plot_rw
+
+Titles = {'Random Walk', 'Vertical', 'Horizontal'};
+figure
+hold on
+for j = 1:3
+subplot(1,3,j)
+imagesc(reshape((Response{j}),12,12))
+set(gca, 'FontSize',16)
+title(Titles{j}, 'FontSize',16)
+xlabel('Tone Frequency', 'FontSize',16)
+ylabel('Click Frequency', 'FontSize',16)
+end
+
+set(gcf,'position',[35 390 1530 410])
+
+
+for j = 1:3
+figure
+b = bar3(reshape((Response{j}),12,12));
+for k = 1:length(b)
+    zdata = b(k).ZData;
+    b(k).CData = zdata;
+    b(k).FaceColor = 'interp';
+end
+set(gca, 'FontSize',16)
+title(Titles{j}, 'FontSize',16)
+xlabel('Tone Frequency', 'FontSize',16)
+ylabel('Click Frequency', 'FontSize',16)
+end
+
+end
+%figure
+% hold on
+% for j = 1:3
+% subplot(2,3,j+3)
+% imagesc(reshape((Rewards{j}),12,12))
+% end
+
+if plot_tr
+    
+figure
+hold on
+errorbar(-60:1:59,mean(App_Tr)*100,ste(App_Tr)*100)
+stairs(-.5 + -60:1:59, -10 + mean(App_Tr_Rew)*20,'k','linewidth',1.5)
+set(gca, 'XLim', [-60 59],'FontSize',16, 'box','off')
+plot([0 0], [0 60], '--k')
+xlabel('Trials - Centered at Reward', 'FontSize',16)
+ylabel('Response Percentage','FontSize',16)
+
+end
+
+if plot_rw
+    
+figure
+hold on
+errorbar(-60:1:59,mean(App_RW)*100,ste(App_RW)*100)
+stairs(-.5 + -60:1:59, -10 + mean(App_RW_Rew)*20,'k','linewidth',1.5)
+set(gca, 'XLim', [-60 59], 'FontSize',16, 'box','off')
+plot([0 0], [0 60], '--k')
+xlabel('Trials - Centered at Reward', 'FontSize',16)
+ylabel('Response Percentage','FontSize',16)
+
+end
+
+end
+
+    function [Resp,Rew] = Response_MAT(MAT)
+    Blocks = {'R','V','H'};
+    use_range = false(size(MAT.Resp_Perc));
+    use_range(1:144*6) = true;
+    use_range = true(size(MAT.Resp_Perc));
+    for j = 1:3
+        Resp{j} = [];
+        Rew{j} = [];
+        bl = strncmp(Blocks{j},MAT.Block,1);
+        for i = 1:144
+            %Edit this to get all trials
+            v = MAT.Vertices == i;
+            
+            Resp{j}(i) = mean(MAT.Resp_Perc(bl & v & use_range));
+            Rew{j}(i) = mean(MAT.Rewards(bl & v & use_range));
+        end        
+        
+    end    
+    
+    end
+
+    function [Resp,Rew] = Approach_MAT(MAT,Blocks)
+    %Blocks = {'V','H'};
+    
+    rew_inds = find(MAT.Rewards);
+    n = 0;
+    for i = rew_inds
+        
+        %Skip random walk blocks
+        if ~any(strncmp(MAT.Block(i),Blocks,1))
+            continue
+        end
+        n = n + 1;
+        if i < 61 || i > numel(MAT.Resp_Perc) - 59
+        else
+            n = n + 1;
+            Resp(n,:) = MAT.Resp_Perc(i-60:i+59);
+            Rew(n,:) = MAT.Rewards(i-60:i+59);
+        end    
+    end
+    
+    end 
+    
